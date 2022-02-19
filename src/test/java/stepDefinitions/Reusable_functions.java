0ece4fe7c16e00cc24672699b87c6c108c081b80
vukhiemnb <vukhiemnb@gmail.com>
2022-02-20 00:01:39 +0700

    add jsonpath to handle nest json response

--- src/test/java/stepDefinitions/Reusable_functions.java
+++ src/test/java/stepDefinitions/Reusable_functions.java
@@ -16,6 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -71,23 +72,27 @@ public class Reusable_functions {
 	
 	public static void api_response_from_get() {
 		//make sure you installed rest assured 
+		String bear_token	= "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGVjMmJlN2IzMGVjNjNmODdlNjUyNTY3N2QwOTczNmM0NzcyMDY0M2ZiMmU3YmIxYmI5ODdiOTBkM2RhOWIwODA4Y2M4Nzc4OGFiYjhmNDAiLCJpYXQiOjE2NDUyODY3NjcsIm5iZiI6MTY0NTI4Njc2NywiZXhwIjoxNjQ2NTgyNzY2LCJzdWIiOiI0NDYiLCJzY29wZXMiOltdfQ.Wp9sjHT63Ke_N6HokcPMK4onV6i-jrgxE65Bue16h4vGH-ZF1cR4WdHvD-KZnxiTaRHSzxsHGkzF8H1lEZX1nXe6PR3Nc6znruTHkFR1sGhKm1Nvjm8VlyAuNopXh9QqAqg9uzByInK411t5rjUG7--DE23nrrLq70GckJwGtlN9wSEQot4eC0Oa32vUYgh29qCFRklGDSIATN6AIR7-ttrjaTBqIV7FqkUl_T2egwr7DPT_rPBQUMMzNx1_vXI2VVyUkS7R0HCWNYgJtk4dNjyEHD1ciRILrgztWKZFQjZCIR3Sp19hoKa7y9l7jfWLfglDCzVreWTf9WDoVNfuSHaKcV6y0MybkYEaJiAFZ-gvgNiJVQFNjmBNLdCkf_bPN02pX0Q7uIJxeqKGHuubAdQznlnBbG17l914gZtmt3rEfy-64iid-gyVVAH7H61W1Je_yUKynGMdhAFHGAHfsqB9qEUYSrj20JRFvG_dRAFYfq-mjNf2ttYNILxSUXWsCk1Wo8SEQEQQup7MWNkzCyDU8YZyBV3cY_S3x6JbptooQwDO2tVR-f8Wy_39mBtZXzMpoBiMmKttsMkb-1KJz2I4_zpW4dY4gRnPadh1pTRYvmeXDRpy9MS0SUkZqheEcQbQFmctsgez2L0pPtTUVuH-mXJdltIjPnAT9HTUDoI";
 		
 		RestAssured.baseURI = "https://factorx-api.test6.amelacorp.com";
-		RequestSpecification API_me = RestAssured.given();
-//		GET request structure
-		
-		String bear_token	= "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjYwZjJkMTc5NjBmNmZlZjAyMWY5MzQ4MzY2YzdjMzczYmRhOWY4ZTdjMWZmY2U0ODAzNTY5ZWU0YzI4Yjg5NzJlNjk0MDhkYzU1Y2RhM2QiLCJpYXQiOjE2NDUxNzkyNTIsIm5iZiI6MTY0NTE3OTI1MiwiZXhwIjoxNjQ2NDc1MjUyLCJzdWIiOiI1MzEiLCJzY29wZXMiOltdfQ.fND_bXdi4YsxwqroWElY9DuzgELCmvtF2Q-NggPt5EAb7I5rUG-g64CBZWi-Sjg-er6DGAU5ZgBKaWVVeimGmBjUVaAYgd7Vb3r96sAf7A139VUcGiwmPAzgWZ2NDbrmef3i9WAwYoDe8AWc0FbgpDUeVXVfTxsHuar7MAUnaFPzPwDPOYfq9mI-0BJ8DrCuRG-CVnW-HXJn_Qk3uBoR0wBB4IxjKAR_o3OEwzy3WHERxZz6yWgAcmPEtO6iWP_rB_e02Eg8nEU4VXR7CFAt0Ts9wGNE5cLUZhRS93oPUXO22JuT9fx7WeR1-FW68mDsBpassSlyX_oRgw-1PpyExSjapZMuGKFZiUbCiGLqk5mmqdhOl4s92rHgB6NKZOm00X3D3X32L7FUylEY_W39SdTZ3xQiIZ4xuoOtBEbsYSBCrcPg9RVhS9qwX1gPmqV5tyMCzbiJ5FaEEaLCPXQXazdVNgMXm4X2yWx2zdhCAZnzIEUr90xDonjqZlCaW2skFdCzPl_lXZKRC7OMm31ZLcNcZRdWzV3UETEz07uLYR3qGfp5rJNoUdGWTzRpp8IpdZaaKaYdktEcfm7yENPxwXQy4j7tHxruRV_tk4wKSoSTOkRvjM7VhUmRPzETgVrZTbJC_HxDmE5hX0gwOCKCGu6OhfzKwe0tLqs6Tlvtl0c";
-		Response response =  API_me
+		RequestSpecification API_me = RestAssured
+				.given()
 				.header("Authorization",
-							"Bearer "+ bear_token)
-		         .contentType(ContentType.JSON)
-//		         .pathParam("id", "AskJsd8Sd")
+				"Bearer "+ bear_token)
+		         .contentType(ContentType.JSON);
+//		GET request structure
+		Response response =  API_me
 		         .when()
 		         .get("/api/v1/me");
 	
 		System.out.println(response.asPrettyString());
-		System.out.println(response.getHeader(bear_token));
-		System.out.println("abc");
+		System.out.println(response.getStatusCode());
+		
+		JsonPath response_jpath = new JsonPath(response.asPrettyString());
+		System.out.println(response_jpath.getString("data.id"));
+//		JsonPath response_jpath_2 = new JsonPath(response_jpath.getString("data"));
+//		System.out.println(response_jpath_2);
+//		System.out.println(response_jpath_2.getString("id"));
 
 //		// Specify the base URL to the RESTful web service 
 //		RestAssured.baseURI = "https://factorx-api.test6.amelacorp.com"; 
