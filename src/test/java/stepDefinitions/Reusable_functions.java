0d089f0e359fc600cd8cb6ab060560656311c70b
vukhiemnb <vukhiemnb@gmail.com>
2022-02-20 23:10:18 +0700

    generate function for GET and POST  method

--- src/test/java/stepDefinitions/Reusable_functions.java
+++ src/test/java/stepDefinitions/Reusable_functions.java
@@ -7,7 +7,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.apache.commons.io.FileUtils;
-
+import org.apache.commons.logging.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -26,24 +28,42 @@ public class Reusable_functions {
 //		WebDriver driver ;
 //		System.setProperty("webdriver.chrome.driver",data.Configure_data.browser_driver_path);
 //		driver = new ChromeDriver();
-//		//goto url
+//		//go to url
 //		driver.get("https://www.browserstack.com");
 //		take_screenshot(driver,"D:\\Auto Test\\test images","test");
 		
-		api_response_from_get();
+
+		Response api_login	= api_response_from_post("/api/v1/login", "", "{\r\n"
+				+ "    \"client_id\": 1,\r\n"
+				+ "    \"client_secret\": \"mYTTkjjLheDGRaBTQzQEFkyEpP1UmaXm3TGRNUPz\",\r\n"
+				+ "    \"grant_type\": \"password\",\r\n"
+				+ "    \"username\": \"factory_company_5\",\r\n"
+				+ "    \"password\": \"Password@123\"\r\n"
+				+ "}");
+		//declare token
+		String bear_token	= get_data_from_api_response(api_login, "data.access_token");
+		
+		Response api_payment	= api_response_from_post("/api/v1/payment_infos",bear_token,
+				"{\r\n"
+				+ "    \"payment_info\": {\r\n"
+				+ "        \"bank_id\": 1,\r\n"
+				+ "        \"bank_branch_id\": 1220,\r\n"
+				+ "        \"account_type\": \"普通\",\r\n"
+				+ "        \"account_number\": \"1234567\",\r\n"
+				+ "        \"account_holder_name_kana\": \"ｱ\"\r\n"
+				+ "    }\r\n"
+				+ "}");
+		System.out.println(api_payment.asPrettyString());
 		}
 	
 	
 	/*This function will take screenshot 
 	 * @param webdriver
 	 * @param folder_path_to_save
-	 * @parm name: the main file name
+	 * @param name: the main file name
 	 * @throws Exception
-	 * 
-		Make sure you installed apache on your device
-	 */
-	
-	
+	 * Make sure you installed apache on your device
+	 */	
 	public static void take_screenshot(WebDriver webdriver,String folder_path_to_save, String name) throws Exception{
 		//Convert web driver object to TakeScreenshot
 		TakesScreenshot scrShot =((TakesScreenshot)webdriver);
@@ -59,51 +79,99 @@ public class Reusable_functions {
 		FileUtils.copyFile(SrcFile, DestFile);
 		}
 	
+	
+	/*This function will return current time with format yyyy-mm-dd--hh-mm-ss 
+	 */
 	public static String get_current_time() {
 		//get current_time
 		LocalDateTime current_time_before_formatted 		= LocalDateTime.now();
 		//initiate time format
-		DateTimeFormatter date_time_format		= DateTimeFormatter.ofPattern("yyyy-MM-dd--kk-mm-ss");
+		DateTimeFormatter date_time_format					= DateTimeFormatter.ofPattern("yyyy-MM-dd--kk-mm-ss");
 		//format time to save file
 		String current_time	= current_time_before_formatted.format(date_time_format);
 		
 		return current_time;
 	}
 	
-	public static void api_response_from_get() {
-		//make sure you installed rest assured 
-		String bear_token	= "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOGVjMmJlN2IzMGVjNjNmODdlNjUyNTY3N2QwOTczNmM0NzcyMDY0M2ZiMmU3YmIxYmI5ODdiOTBkM2RhOWIwODA4Y2M4Nzc4OGFiYjhmNDAiLCJpYXQiOjE2NDUyODY3NjcsIm5iZiI6MTY0NTI4Njc2NywiZXhwIjoxNjQ2NTgyNzY2LCJzdWIiOiI0NDYiLCJzY29wZXMiOltdfQ.Wp9sjHT63Ke_N6HokcPMK4onV6i-jrgxE65Bue16h4vGH-ZF1cR4WdHvD-KZnxiTaRHSzxsHGkzF8H1lEZX1nXe6PR3Nc6znruTHkFR1sGhKm1Nvjm8VlyAuNopXh9QqAqg9uzByInK411t5rjUG7--DE23nrrLq70GckJwGtlN9wSEQot4eC0Oa32vUYgh29qCFRklGDSIATN6AIR7-ttrjaTBqIV7FqkUl_T2egwr7DPT_rPBQUMMzNx1_vXI2VVyUkS7R0HCWNYgJtk4dNjyEHD1ciRILrgztWKZFQjZCIR3Sp19hoKa7y9l7jfWLfglDCzVreWTf9WDoVNfuSHaKcV6y0MybkYEaJiAFZ-gvgNiJVQFNjmBNLdCkf_bPN02pX0Q7uIJxeqKGHuubAdQznlnBbG17l914gZtmt3rEfy-64iid-gyVVAH7H61W1Je_yUKynGMdhAFHGAHfsqB9qEUYSrj20JRFvG_dRAFYfq-mjNf2ttYNILxSUXWsCk1Wo8SEQEQQup7MWNkzCyDU8YZyBV3cY_S3x6JbptooQwDO2tVR-f8Wy_39mBtZXzMpoBiMmKttsMkb-1KJz2I4_zpW4dY4gRnPadh1pTRYvmeXDRpy9MS0SUkZqheEcQbQFmctsgez2L0pPtTUVuH-mXJdltIjPnAT9HTUDoI";
-		
-		RestAssured.baseURI = "https://factorx-api.test6.amelacorp.com";
+	/*This function will return response from API GET 
+	 * @param request_uri
+	 * @param request_token
+	 * Make sure you add rest assured dependency
+	 */	
+	public static Response api_response_from_get(String request_uri, String request_token) {				
+		//set base URI
+		RestAssured.baseURI = data.Configure_data.baseURI;		
+		//create request
 		RequestSpecification API_me = RestAssured
-				.given()
-				.header("Authorization",
-				"Bearer "+ bear_token)
-		         .contentType(ContentType.JSON);
-//		GET request structure
+										.given()
+										//set token
+										.header("Authorization",
+										"Bearer "+ request_token)
+										//set content-type
+								        .contentType(ContentType.JSON);
+		//GET request structure
 		Response response =  API_me
-		         .when()
-		         .get("/api/v1/me");
-	
-		System.out.println(response.asPrettyString());
-		System.out.println(response.getStatusCode());
+		         				.when()
+		         				//add method and send request
+		         				.get(request_uri);
 		
-		JsonPath response_jpath = new JsonPath(response.asPrettyString());
-		System.out.println(response_jpath.getString("data.id"));
-//		JsonPath response_jpath_2 = new JsonPath(response_jpath.getString("data"));
-//		System.out.println(response_jpath_2);
-//		System.out.println(response_jpath_2.getString("id"));
-
-//		// Specify the base URL to the RESTful web service 
-//		RestAssured.baseURI = "https://factorx-api.test6.amelacorp.com"; 
-//		// Get the RequestSpecification of the request to be sent to the server. 
-//		RequestSpecification API_me = RestAssured.given(); 
-//		// specify the method type (GET) and the parameters if any. 
-//		//In this case the request does not take any parameters 
-//		Response response = API_me.request(Method.GET, "");
-//		// Print the status and message body of the response received from the server 
-//		System.out.println("Status received => " + response.getStatusLine()); 
-//		System.out.println("Response=>" + response.prettyPrint());
+		return(response);
+	}
+	
+	/*This function will return value of a key from input JSON response 
+	 * @param response_input
+	 * @param data_path
+	 * Make sure you add JSON dependency
+	 */	
+	public static String get_data_from_api_response(Response response_input, String data_path) {
+		JsonPath response_json_path = new JsonPath(response_input.asPrettyString());
+		String data_output			= response_json_path.getString(data_path);
+		return data_output;
+	}
+	
+	/*This function will return value of a key from input JSON response 
+	 * @param request_uri
+	 * @param request_token
+	 * @param request_body
+	 * Make sure you add rest assured and JSON dependency
+	 */	
+	public static Response api_response_from_post(String request_uri, String request_token, String request_body) {
+		//make sure you installed rest assured
+		
+		//set base URI
+		RestAssured.baseURI = data.Configure_data.baseURI;
+		
+		//set request param
+		//make sure you add json dependency		
+		JSONObject request_body_Obj	= convert_json_from_string(request_body);
+		
+		//create request
+		RequestSpecification API_me = RestAssured
+										.given()
+										//set token
+										.header("Authorization",
+										"Bearer "+ request_token)
+										//set content-type
+								        .contentType(ContentType.JSON);										
+		
+//		POST request structure
+		Response response =  API_me
+		         				.when()
+		         				//add body
+		         				.body(request_body_Obj.toString())
+		         				//add method and send request
+		         				.post(request_uri);		
+		return response;
+	}
+	
+	/*This function will return JSON from input JSON string input
+	 * @param input_json_string
+	 * Make sure you add JSON dependency
+	 */	
+	public static JSONObject convert_json_from_string(String input_json_string) {
+		    JSONObject jsonObject 	= new JSONObject(input_json_string);
+		    return jsonObject;
+		
 	}
 	
 }
