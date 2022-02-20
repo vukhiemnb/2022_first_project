617e5bf9696a3e67e7ca4e853684bdacf3bebbac
vukhiemnb <vukhiemnb@gmail.com>
2022-02-20 23:10:18 +0700

    generate function for GET and POST  method

--- src/test/java/data/Configure_data.java
+++ src/test/java/data/Configure_data.java
@@ -4,4 +4,6 @@ public class Configure_data {
 	public static String base_url = "https://factorx.test6.amelacorp.com";
 	
 	public static String browser_driver_path	= "D://selenium jars//chromedriver.exe";
+	
+	public static String baseURI				= "https://factorx-api.test6.amelacorp.com";
 }
