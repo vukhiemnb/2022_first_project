e1bf9f6fb33519a7634886719259796d98ecc4b8
vukhiemnb <vukhiemnb@gmail.com>
2022-03-21 23:33:10 +0700

    add webdriver manager

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -3,12 +3,13 @@ package stepDefinitions;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks {
 	public WebDriver driver;
 	@Before
 	public void Declare_driver() {
-		System.setProperty("webdriver.chrome.driver", data.Configure_data.browser_driver_path);
+		WebDriverManager.chromedriver().setup();
 	}
 
 }
